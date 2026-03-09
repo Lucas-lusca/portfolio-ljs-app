8abcfa77c199132cc53fd2d936cf28d9159b674e
Lucas JS <94575921+Lucas-lusca@users.noreply.github.com>
2026-03-09 17:29:15 -0300

    Adicionado Footer
    Footer adicionado e pequenas alteracoes de layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterLayout } from "./shared/footer-layout/footer-layout";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterLayout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/shared/footer-layout/footer-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-layout',
+  imports: [],
+  templateUrl: './footer-layout.html',
+  styleUrl: './footer-layout.css',
+})
+export class FooterLayout {}
